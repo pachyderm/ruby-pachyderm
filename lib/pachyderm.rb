9e2197cadb978c7937f4a58fb2a9545fe3c618d6
Sean Jezewski <seanwjezewski@gmail.com>
2018-05-25 17:31:54 -0700

    Add streaming test

--- lib/pachyderm.rb
+++ lib/pachyderm.rb
@@ -5,37 +5,5 @@ require 'client/version/versionpb/version_services_pb'
 
 module Pachyderm
     class << self
-
-        def new_client
-
-        end
-
     end
-
-	class Client
-
-		def gogogo
-
-			stub = Pfs::API::Stub.new('127.0.0.1:30650',
-											   :this_channel_is_insecure)
-			req = Pfs::ListRepoRequest.new()
-			puts "req: #{req}\n"
-
-			resp = stub.list_repo(req)
-
-			puts "resp: #{resp}\n"
-			resp.repo_info.each do |repo_info|
-				puts "#{repo_info.repo.name}"
-			end
-
-		end
-
-	end
-
 end
-
-
-c = Pachyderm::Client.new
-
-c.gogogo()
-
--- test/test.rb
+++ test/test.rb
@@ -41,9 +41,6 @@ class TestClientConnection < Minitest::Test
 		client = Pfs::API::Stub.new($address, :this_channel_is_insecure)
 		req = Pfs::ListRepoRequest.new()
 		resp = client.list_repo(req)
-		resp.repo_info.each do |repo_info|
-			puts "#{repo_info.repo.name}"
-		end
 		assert_equal 0, resp.repo_info.size
 	end
 
@@ -60,13 +57,48 @@ class TestClientConnection < Minitest::Test
 		assert_match(/repo\sname\s.*?invalid/, err.to_s)
     end
 
-    def test_streaming_response
-    end
+    def test_streaming_calls
+		delete_all
+		client = Pfs::API::Stub.new($address, :this_channel_is_insecure)
+		repo_name = "foo"
+		content = "yabba dabba doo"
 
-    def test_pfs
-    end
+		# Create some content in a commit
+		req = Pfs::CreateRepoRequest.new(:repo=>Pfs::Repo.new(:name =>repo_name))
+		client.create_repo(req)
+		repo = Pfs::Repo.new(:name =>repo_name)
+		path = "/bar"
+	
+		commit = client.start_commit(Pfs::StartCommitRequest.new(
+			:branch => "master",
+			:parent => Pfs::Commit.new(
+				:repo => repo,
+			)
+		))
 
-    def test_pps
+		req = Pfs::PutFileRequest.new(
+			:file => Pfs::File.new(
+				:commit => commit,
+				:path => path
+			),
+			:value => content
+		)
+		client.put_file([req].each)
+
+		client.finish_commit(Pfs::FinishCommitRequest.new(
+			:commit => commit
+		))
+
+		req = Pfs::GetFileRequest.new(
+			:file => Pfs::File.new(
+				:commit => commit,
+				:path => path
+			)
+		)
+		resps = client.get_file(req)
+		resps = resps.collect {|i| i}
+		assert_equal 1, resps.size
+		assert_equal content, resps.first.value
     end
 
     def test_auth
