1b7be8f60f62bf3a5341d7d79e910e02e7f5b208
Sean Jezewski <seanwjezewski@gmail.com>
2018-05-25 17:48:23 -0700

    Outline test case

--- test/auth.rb
+++ test/auth.rb
@@ -6,15 +6,10 @@ $address = "127.0.0.1:30650"
 class TestClientConnection < Minitest::Test
 
     def test_auth
-		client = Versionpb::API::Stub.new($address, :this_channel_is_insecure)
-		req = Google::Protobuf::Empty.new
-		resp = client.get_version(req)
-		version = "#{resp.major}.#{resp.minor}.#{resp.micro}"
-		unless resp.additional == ""
-			version += "#{resp.additional}"
-		end
-		expected_version = File.read("VERSION").strip
-		assert_equal expected_version,version
+        # $ pachctl auth activate --initial-admin=robot:admin
+		# negative control : shouldn't be able to list-admins w a client w no token
+		#$ echo "${ADMIN_TOKEN}" | pachctl auth use-auth-token # authenticates you as the cluster admin
+		# positive control : should be able to list admins
     end
 
 
